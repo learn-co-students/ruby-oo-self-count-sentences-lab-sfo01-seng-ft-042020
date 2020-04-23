9734132072d6eb3e588109af3b04ec294f0b8bf9
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-04-23 14:35:19 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,37 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    if(self.include?".")
+      return true
+    else 
+      return false
+    end 
   end
 
   def question?
-
+    if(self.include?"?")
+      return true
+    else 
+      return false
+    end 
   end
 
   def exclamation?
-
+    if(self.include?"!")
+      return true
+    else 
+      return false
+    end 
   end
 
   def count_sentences
-
-  end
+    num = 0
+    num = self.split(/[\.!?]/).grep(/\S/).count
+    if(num > 0)
+      return num
+    else 
+      return 0
+    end 
+  end 
 end
