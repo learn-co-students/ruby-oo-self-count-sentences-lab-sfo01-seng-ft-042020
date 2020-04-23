462d8af62d718ad99a294cb773c17abdc2441819
Seanbon0611 <seand0611@gmail.com>
2020-04-23 14:09:01 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.include?('.')
+      return true
+    else 
+      return false
+    end
   end
 
   def question?
-
+    if self.include?('?')
+      return true
+    else 
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.include?('!')
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
-
+    # binding.pry
+    self.split(/[.!]/).length
   end
 end
